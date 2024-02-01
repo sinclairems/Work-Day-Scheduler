927467f504b8f5be1afa70793d261e9a6423044f
Erin <50000677+sinclairems@users.noreply.github.com>
2024-01-31 18:43:15 -0600

    Final push with new code

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,74 +1,69 @@
 // ** Pseudocode ** //
 // Use dayjs to display the current date and time in the header
-// Create time blocks for each hour of the work day
-// Create a text area for the user to enter tasks/meetings/events
-// Create a button to save the text
 // Add a listener for click events on the save button
-// Use the id in the containing time-block as a key to save the user input in local storage
-// When the page is refreshed, get any user input that was saved in localStorage and set the values of the corresponding textarea elements
-// Add code to apply the past, present, or future class to each time block by comparing the id to the current hour
+// Add code to apply the past, present, or future class to each time
+// block by comparing the id to the current hour.
+// Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements.
 
+// ** Code ** //
 
-// Variables
-var currentDayEl = $("#currentDay");
-var containerEl = $(".container");
+// Display today's day and date
+var now = dayjs().format('MMMM DD, YYYY [at] hh:mm A');
+$("#currentDay").html(now);
 
-var now = dayjs();
+// Save button click listener
+$(function () { 
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
+        localStorage.setItem(time, text);
+    })
+  
+// Add past, present, or future class to each time block
+    function timeTracker() {
+        var timeNow = dayjs().hour();
 
-var timeBlocks = [
-    { hour: "9", time: "9:00 AM" },
-    { hour: "10", time: "10:00 AM" },
-    { hour: "11", time: "11:00 AM" },
-    { hour: "12", time: "12:00 PM" },
-    { hour: "13", time: "1:00 PM" },
-    { hour: "14", time: "2:00 PM" },
-    { hour: "15", time: "3:00 PM" },
-    { hour: "16", time: "4:00 PM" },
-    { hour: "17", time: "5:00 PM" },
-];
-var timeBlockEl = $(".time-block");
-var saveBtn = $(".saveBtn");
-var textArea = $("textarea");
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
 
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
 
-// Use this for style:
-{/* <div id="hour-9" class="row time-block past">
-            <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
-            <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
-            <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-                <i class="fas fa-save" aria-hidden="true"></i>
-            </button>
-        </div> */}
+            }
+        })
+    }
 
+// Get any user input that was saved in localStorage and set the values of the corresponding textarea elements
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-// Functions
+// Call timeTracker function
+    timeTracker();
+})
 
-
-
-
-
-
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
-
+// source: https://github.com/sylviaprabudy/work-day-scheduler
 
 
 
